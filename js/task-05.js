692208f8fadfdf8a0ba0f77967aa2da1e7a1b055
Dimentiy88 <bodartinov88@gmail.com>
2023-07-17 21:01:23 +0200

    Task - 04

---
+++ js/task-05.js
@@ -0,0 +1,15 @@
+// const inputRef = document.querySelector("#name-input");
+// const nameRef = document.querySelector("#name-output");
+
+// const getInputValue = ({ currentTarget }) => {
+//   if (currentTarget.value.trim() !== "") {
+//     nameRef.textContent = currentTarget.value.trim();
+//   } else {
+//     nameRef.textContent = "Anonymous";
+//   }
+// };
+
+// inputRef.addEventListener("input", getInputValue);
+
+const input = document.getElementById("name-input");
+const span = document.getElementById("name-output");
