83f489fb9617314f79104c76b32e809880368501
Dimentiy88 <bodartinov88@gmail.com>
2023-07-21 21:47:19 +0200

    end-task-2/7

--- js/task-02.js
+++ js/task-02.js
@@ -7,12 +7,14 @@ const ingredients = [
   "Condiments",
 ];
 
-const ulEl = document.getElementById("ingredients");
-const liEl = ingredients.forEach((ingredient) => {
-  const liEl = document.createElement("li");
-  liEl.textContent = ingredient;
-  ulEl.append(liEl);
-  ulEl.classList.add("item");
+const ulEL = document.querySelector("#ingredients");
+
+const listEL = ingredients.map((ingredient) => {
+  const item = document.createElement("li");
+  item.textContent = ingredient;
+  item.classList.add("item");
+  return item;
 });
 
+ulEL.append(...listEL);
 console.log(ingredients);
--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,6 @@
 const inputControl = document.getElementById("font-size-control");
 const abracadabra = document.getElementById("text");
 
-inputControl.addEventListener("change", (e) => {
+inputControl.addEventListener("input", (e) => {
   abracadabra.style.fontSize = `${e.target.value}px`;
 });
